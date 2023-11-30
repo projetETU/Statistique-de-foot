c15052df7859e47d898d2b65115a9572a1bce66e
Mendrika Rs <men.malala@gmail.com>
2023-11-30 12:51:40 +0300

    Update base.sql

--- base.sql
+++ base.sql
@@ -9,4 +9,31 @@ CREATE TABLE match(
     id_competition INT ,
     FOREIGN KEY (id_competition) REFERENCES competition(id_competition)
 );
-CREATE TABLE detailsMatch();
+CREATE TABLE equipe(
+    id_equipe INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR
+);
+CREATE TABLE lieu(
+    id_lieu INT AUTO_INCREMENT PRIMARY KEY,
+    lieu VARCHAR(20)
+);
+INSERT INTO lieu(lieu) VALUES ("DOMICILE");
+INSERT INTO lieu(lieu) VALUES ("EXTERIEUR");
+
+CREATE TABLE detailsMatch(
+    id_details INT AUTO_INCREMENT PRIMARY KEY,
+    id_match INT ,
+    id_competition INT ,
+    id_equipe INT,
+    but INT ,
+    cartonJ INT ,
+    cartonR INT,
+    possesion% float ,
+    passeReussi% float,
+    aerienGagner INT ,
+    id_lieu INT ,
+    note INT ,
+    FOREIGN KEY (id_competition) REFERENCES competition(id_competition),
+    FOREIGN KEY (id_equipe) REFERENCES equipe(id_equipe),
+    FOREIGN KEY (id_lieu) REFERENCES lieu(id_lieu)
+);
